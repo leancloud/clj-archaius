91760e7bd3c14ca5a939afb09dbc80ba78dcf0e0
liuchong <mail@liuchong.io>
2021-03-19 15:55:11 +0800

    (chore) bump version 0.4.1

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject cn.leancloud/clj-archaius "0.4.0"
+(defproject cn.leancloud/clj-archaius "0.4.1"
   :description "A Clojure library designed to use Netflix/archaius for configuration management."
   :url "https://github.com/leancloud/clj-archaius"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [cn.leancloud/archaius-core "0.7.8-a"]]
+                 [cn.leancloud/archaius-core "0.7.8-b"]]
   :profiles {:dev {:resource-paths ["dev"]}})
